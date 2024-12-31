da37e9e23bf698d233512426ca1b85fae579d2c2
root <root@saikvm.us-central1-f.c.premium-lead-445300-r3.internal>
2024-12-31 12:15:35 +0000

    sai new docker file commit

--- springboot-thymeleaf-crud/src/main/java/net/javaguides/springboot/controller/Demo.java
+++ springboot-thymeleaf-crud/src/main/java/net/javaguides/springboot/controller/Demo.java
@@ -3,24 +3,45 @@ package net.javaguides.springboot.controller;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import java.util.Map;
 
 @RestController
 public class Demo {
-	@Value("${spring.datasource.url}")
-	private String db_url = System.getenv("db_url");
-
-	@Value("${spring.datasource.username}")
-    private String db_username = System.getenv("db_username");
-
-	 
-    @Value("${spring.datasource.password}")
-    private String db_pasword = System.getenv("db_password");
     
-    //@GetMapping("/demo")
-    //public Databaseinfo demo() {
-       // return new Databaseinfo(db_url, db_username, db_password);
-    //}
+    @Value("${spring.datasource.url}")
+    private String dbUrl;
+    
+    @Value("${spring.datasource.username}")
+    private String dbUsername;
+    
+    @Value("${spring.datasource.password}")
+    private String dbPassword;
+
+    @GetMapping("/demo")
+    public String demo() {
+        return String.format("DB URL: %s, Username: %s, Password: %s", dbUrl, dbUsername, dbPassword);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     
       
-}
+
